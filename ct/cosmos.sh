b828f02daeaed8d3f7f5421b365baacaa947231a
Smiley83460 <59771027+Smiley83460@users.noreply.github.com>
2024-10-15 09:36:58 +0200

    Update cosmos.sh

--- ct/cosmos.sh
+++ ct/cosmos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/Smiley83460/personalcmscloud/refs/heads/main/build.func)
+source <(curl -s https://raw.githubusercontent.com/Smiley83460/my-helper-script/refs/heads/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
